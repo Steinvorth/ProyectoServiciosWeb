8bd0536505e4fc09d3f1cdec9e0ccb4debf0988e
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-07-16 19:02:42 -0600

    Create PurisFlashController.cs

---
+++ ProjectoFinalServiciosWeb/ProjectoFinalServiciosWeb/Controllers/PurisFlashController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectoFinalServiciosWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurisFlashController : ControllerBase
+    {
+    }
+}
